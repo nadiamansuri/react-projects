e116f2b5e68b27f7fb359bccbb087b70302c818f
nadiaalamgir21 <nmansuri@lunchbox.io>
2022-07-04 15:33:04 -0700

    grocery bud project compelted with no bugs

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,15 @@
 import React, { useEffect } from 'react'
 
-const Alert = ({type, msg}) => {
-  return <h2>alert component</h2>
+const Alert = ({type, msg, removeAlert, list}) => {
+    useEffect(()=>{
+        const timeout = setTimeout(()=>{
+            removeAlert();
+        }, 3000)
+        return () => clearTimeout(timeout);
+    }, [list]);
+return <p className={`alert alert-${type}`}>{msg}</p>;
+//  ^ BASED ON THE ALERT IT WILL USE THE STYLING OF SUCCESS OR DANGER, TOGGLE STYLING!! PRETTY COOL
+  
 };
 
 export default Alert;
--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,43 +3,92 @@ import List from './List'
 import Alert from './Alert';
 
 
+const getLocalStorage = () =>{
+  let list = localStorage.getItem('list')
+  if(list){
+    // STORING IN STRING SO WILL HAVE TO STRINGIFY IT
+    return JSON.parse(localStorage.getItem('list'))
+  } else {
+    return []
+  }
+}
+
 function App() {
   const [name, setName] = useState('')
-  const [list, setList] = useState([])
+  const [list, setList] = useState(getLocalStorage())
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
-  const [alert, setAlert] = useState({show: true, msg: 'Hello World', type: 'success'})
+  const [alert, setAlert] = useState({show: false, msg: '', type: ''})
 
 const handleSubmit = (e) =>{
-  e.preventDefault()
+  e.preventDefault();
   // EMPTY STRING ALERT//
   if(!name){
-
-//  empty string '' is a falsely value so the if(!name) basically is name is false as in empty thn display alert  . . . . . so n so
+  showAlert(true, 'danger', 'Please enter value');
+//  EMPTY STRING '' IS A FALSY VALUE SO IF(!NAME) BASICALLY IF NAME IS FALSE AS IN EMPTY THN ALERT  . . . . . SO AND SO
   }
+
 // CHECK FOR VALUE AND EDITING ALERT
-  else if(name && isEditing){
  // this is for if name is true as in if there is any value and not just empty submission in the grocery list and also if the value is being edited, then we alert so both the conditions are met thn we deal with this.
+
+  else if(name && isEditing){
+setList(
+  list.map((item)=>{
+    if(item.id === editID){
+      return { ...item, title: name}
+    }
+    return item;
+  })
+);
+setName('');
+setEditID(null);
+setIsEditing(false);
+showAlert(true, 'success', 'value changed');
+
   }
 
 // IF EVERYTHING GOES WELL AND WE HAVE VALUE IN OUR LIST THN BOTTOM FUNCTION WILL RUN
   // NEW ITEM TO THE LIST (VALUE TAKEN AND ADDED AND DISPLAY ALONG WITH OLD LIST.)
   else{
-    const newItem = {id: new Date().getTime().toString(), title:name};
-    setList([...list, newItem])
-    // RIGHT AFTER VALUE IS SUBMITTED, INPUT WILL BE CLEARED BACK TO EMPTY STRING ''
-    setName('')
+    showAlert(true, 'success', 'item added to the list');
+    const newItem = {id: new Date().getTime().toString(), title: name};
+
+      // RIGHT AFTER VALUE IS SUBMITTED, INPUT WILL BE CLEARED BACK TO EMPTY STRING ''
+    setList([...list, newItem]);
+    setName('');
   }
+};
+
+// MADE A FUNCTION FOR ALERT INSTEAD BUT CANT FIGURE OUT THE PURPOSE FOR IT
+const showAlert = (show = false,  type =  '', msg = '') =>{
+  setAlert({show, type, msg});
 }
 
+const clearList = () =>{
+  showAlert(true, 'danger', 'empty list');
+  setList([]);
+}
 
+const removeItem = (id) =>{
+  showAlert(true, 'danger', 'item removed');
+  setList(list.filter((item) => item.id !==id));
+};
 
+const editItem = (id) =>{
+  const specificItem = list.find((item) => item.id === id);
+  setIsEditing(true);
+  setEditID(id);
+  setName(specificItem.title);
+}
+useEffect(()=>{
+  localStorage.setItem('list', JSON.stringify(list))
+}, [list])
 
   return (
   <section className='section-center'>
 
 <form className='grocery-form' onSubmit={handleSubmit}>
-  {alert.show && <Alert {...alert}/>}   
+  {alert.show && <Alert {...alert} removeAlert={showAlert} list={list}/>}   
                             {/* ^ OBJECT THREAD OPERATOR*/}
   <h3>Grocery Bud</h3>
   <div className='form-control'>
@@ -55,9 +104,9 @@ const handleSubmit = (e) =>{
 {/* CONDITION APPLIED FOR THE CLEAR BTN TO SHOW ONLY WHEN THERE IS VALUE IN THE LIST ALONG WITH EDIT OR DELETE BTN */}
 {list.length > 0 && (
   <div className='grocery-container'>
-  <List items={list}/>
+  <List items={list} removeItem={removeItem} editItem={editItem}/>
   {/* ^PROP IS = LIST STATE VALUE */}
-  <button className='clear-btn'>Clear Items</button>
+  <button className='clear-btn' onClick={clearList}>Clear Items</button>
 </div>
 )}
 
