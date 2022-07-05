9e46891b00812dec29f88f4f2133388f0565b4e9
nadiaalamgir21 <nmansuri@lunchbox.io>
2022-07-04 17:48:09 -0700

    Sidebar-modal structure created

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,18 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) =>{
+    return <AppContext.Provider value='hello'>
+        {children}
+    </AppContext.Provider>
+
+    
+    }
+    // CUSTOM HOOK ---- IF YOU WANT TO USE REACT HOOK YOU HAVE TO EITHER NEEDS TO BE A COMPONENT OR A CUSTOM HOOK AND CUSTOM HOOK IS ALWAYS USED WITH "USE......"
+    export const useGlobalContext = () =>{
+        return useContext(AppContext)
+
+}
+
+export {AppContext, AppProvider}
